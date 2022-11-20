ae4edc0e2edb8daf429d6441e4c245583024320c
knt <khinnandar.tun@wavemoney.com.mm>
2022-11-20 13:19:46 +0630

    Initial commit

---
+++ dynamodb-s3_test.go
@@ -0,0 +1,26 @@
+package main
+
+// import (
+// 	"testing"
+
+// 	"github.com/aws/aws-cdk-go/awscdk/v2"
+// 	"github.com/aws/aws-cdk-go/awscdk/v2/assertions"
+// 	"github.com/aws/jsii-runtime-go"
+// )
+
+// example tests. To run these tests, uncomment this file along with the
+// example resource in dynamodb-s3_test.go
+// func TestDynamodbS3Stack(t *testing.T) {
+// 	// GIVEN
+// 	app := awscdk.NewApp(nil)
+
+// 	// WHEN
+// 	stack := NewDynamodbS3Stack(app, "MyStack", nil)
+
+// 	// THEN
+// 	template := assertions.Template_FromStack(stack)
+
+// 	template.HasResourceProperties(jsii.String("AWS::SQS::Queue"), map[string]interface{}{
+// 		"VisibilityTimeout": 300,
+// 	})
+// }
